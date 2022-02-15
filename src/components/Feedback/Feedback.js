3ea30c83ddd93a12b8a3af4715b01af9403f5ef3
OlgaKhlivnenko <xlivnenko@ukr.net>
2022-02-15 13:06:03 -0500

    feat: input

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
+import './Feedback.css';
 class Feedback extends React.Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
     total: 0,
-    positivefeedback: 0,
   };
   
   goodFeedback = () => {
@@ -29,33 +28,53 @@ class Feedback extends React.Component {
         bad: prevState.bad + 1,
       };
     });
+  };
+  
+  countTotalFeedback = () => {
+    this.setState(prevState => {
+        
+        return {
+          total: this.state.bad + this.state.neutral + this.state.good,
+      };
+      
+    });
+    console.log(this.total)
     };
-  // countTotalFeedback()
+  
   // countPositiveFeedbackPercentage()
     render() {
     return (
-      <div>
+      <div className='conteiner'>
         <h1>Please leave feedback</h1>
         <div>
-          <button type="button" onClick={this.goodFeedback}>
+          <button className='statistic-btn' type="button" onClick={evt => {
+            { this.goodFeedback }
+          }}>
+            
             Good
           </button>
-          <button type="button" onClick={this.neutralFeedback}>
+          <button className='statistic-btn' type="button" onClick={this.neutralFeedback}>
             Neutral
           </button>
-          <button type="button" onClick={this.badFeedback}>
+          <button className='statistic-btn' type="button" onClick={this.badFeedback}>
             Bad
           </button>
         </div>
         <h2>Statistics</h2>
-        <div>
-          <span>Good: {this.state.good}</span>
-          <span>Neutral: {this.state.neutral}</span>
-          <span>Bad: {this.state.bad}</span>
+        <div className='statistic-box'>
+          <span className='statistic--item'>Good: {this.state.good}</span>
+          <span className='statistic--item'>Neutral: {this.state.neutral}</span>
+          <span className='statistic--item'>Bad: {this.state.bad}</span>
+          <span className='statistic--item'>Total: {this.state.total}</span>
+          <span className='statistic--item'>Positive feedback: {this.state.positiveFeedback}</span>
         </div>
       </div>
     );
   }
+};
+Feedback.propTypes = {
+  
 }
+
 
 export default Feedback;
